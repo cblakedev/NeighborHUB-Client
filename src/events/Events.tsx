6d37470c8550f20e7171e38a4f84e31c561bb43e
Chris Blake <cblake2335@gmail.com>
2021-09-29 21:30:25 -0400

    Added events to the main home page from fetched information from the Ticketmaster API. Minor styling added.

--- src/events/Events.tsx
+++ src/events/Events.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Form, Button} from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AiFillPlusCircle } from 'react-icons/ai'
 
 type EventsProps = {
 
@@ -18,29 +19,59 @@ class Events extends Component<EventsProps, EventsState> {
         }
     }
 
+    timeConvert = (date: string): string => {
+        const time = new Date(date).toLocaleTimeString('en',
+            { timeStyle: 'short', hour12: true, timeZone: 'UTC' });
+
+        return time
+    }
+
+    dateConvert = (date: string): string => {
+        const myDate = new Date(date);
+        return `${(myDate.getMonth() + 1)}-${myDate.getDate()}-${myDate.getFullYear()}`
+    }
+
+
     componentDidMount() {
         fetch(`https://app.ticketmaster.com/discovery/v2/events.json?dmaId=303&sort=date,asc&size=50&apikey=4HGjWgd6muYekVCVQmKoYScIKKbzXbni`)
-        .then((res) => res.json())
-        .then((data) => {
-            this.setState({
-                eventData: data
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({
+                    eventData: data
+                })
+                console.log(this.state.eventData._embedded.events)
             })
-            console.log(this.state.eventData)
-        })
     }
 
     render() {
-        return(
-            <Container className='mainFeedWrapper'>
-                <Row className='feedWrapper'>
-                    <Col> 
-                        <p>Poster goes here</p>
-                        <h3>Event Name goes here</h3>
-                        <p>Date goes here</p>
-                        <p>Event url goes here</p>
-                    </Col>
-                </Row>
-            </Container>
+        return (
+            <>
+                <h2>Upcoming Events</h2>
+                {
+                    this.state.eventData._embedded
+                        ?
+                        this.state.eventData._embedded.events.map((event: any) => {
+                            return (
+                                <Row className='eventWrapper'>
+                                    <Col className='eventItemCol'>
+                                        <img src={event.images[0].url} />
+                                        <h4>{event.name}</h4>
+                                        <span><h5>Date:</h5> {this.dateConvert(event.dates.start.dateTime)}, </span>
+                                        <span><h5>Time:</h5> {this.timeConvert(event.dates.start.dateTime)}</span>
+                                        <div className='eventBtns'>
+                                            <Button href={event.url}>Get Tickets</Button>
+                                            <span><AiFillPlusCircle /></span>
+                                        </div>
+
+                                    </Col>
+                                </Row>
+                            )
+                        })
+                        :
+                        undefined
+                }
+
+            </>
         )
     }
 }
