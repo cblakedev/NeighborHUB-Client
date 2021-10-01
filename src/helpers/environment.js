2b7342dc5435412dd6f3ff4cd0c336788f64d4d7
Chris Blake <cblake2335@gmail.com>
2021-09-30 20:38:30 -0400

    initial deploy build

---
+++ src/helpers/environment.js
@@ -0,0 +1,14 @@
+let APIURL = '';
+
+switch (window.location.hostname) {
+
+    case 'localhost' || '127.0.0.1':
+        APIURL = 'http://localhost:5000/';
+        break;
+
+    default :
+        APIURL = 'https://neighborhub-server.herokuapp.com'
+      
+}
+
+export default APIURL;
